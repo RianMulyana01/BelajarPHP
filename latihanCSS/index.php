71f2a8ce8b70f485c45955734c4881292905a3dc
RianMulyana01 <ferrydwindulasari@gmail.com>
2024-05-08 08:47:44 +0700

    latihanCSS

---
+++ latihanCSS/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glow shadow hover Button</title>
+<link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <a href="#" class="btn">Button</a>
+    <a href="#" class="btn2">Restar</a>
+    <a href="#" class="btn3">Tambah</a>
+    
+</body>
+</html>
