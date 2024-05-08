e15626a3bdb98969178672346a5d6f1e924209b7
RianMulyana01 <ferrydwindulasari@gmail.com>
2024-05-08 08:46:26 +0700

    latihanCSS

---
+++ latihanCSS2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar CSS-Dasar</title>
+<link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container">
+         <div class="ff">
+             <div class="ff-img">
+             <img src="https://i.ytimg.com/vi/fuoySbMJ0H8/maxresdefault.jpg" alt="">
+        </div> 
+
+             <div class="ff-body">
+                <h2 class="ff-jpg">Fast And Furious 2019</h2>
+                <p class="ff-intro">Fast and Furious adalah sebuah film Amerika Serikat yang dirilis pada tahun 2003. 
+                  Film yang disutradarai oleh John Singleton ini pemainnya antara lain ialah Paul Walker, Tyrese Gibson,
+                  Eva Mendes, Cole Hauser, James Remar, Ludacris, dan Devon Aoki. 
+                  Film ini dirilis pada tanggal 6 Juni 2003 di Amerika Serikat dan 23 Agustus 2003 di Jepang. 
+                </p>
+            </div> 
+                </div> 
+            </div> 
+</body>
+</html>
